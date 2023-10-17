26ee88a6028c0d9322740669c3f962da8558c4bb
Simon Bertram <simon.bertram@fxplus.ac.uk>
2023-10-17 09:55:16 +0100

    Create custom error middleware

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,7 +1,9 @@
+import asyncHandler from 'express-async-handler';
+
 // @desc    Auth user & get token
 // route   POST /api/users/auth
 // access  Public
-const authUser = (req, res) => {
+const authUser = async (req, res) => {
   res.status(200).json({ message: 'Login user route' });
 };
 
---
+++ backend/middleware/errorMiddleware.js
@@ -0,0 +1,25 @@
+const notFound = (req, res, next) => {
+    const error = new Error(`Not Found - ${req.originalUrl}`);
+    // 404 is the not found error
+    res.status(404);
+    // pass error to next middleware
+    next(error);
+};
+
+const errorHandler = (err, req, res, next) => {
+    // set status code to 500 if status code is 200
+    let statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+    let message = err.message;
+    // check if error is a CastError and if the 
+    if (err.name === 'CastError' && err.kind === 'ObjectId') {
+      // set status code to 404
+      statusCode = 404;
+      message = 'Resource not found';
+    }
+    res.status(statusCode);
+    res.json({
+        message,
+        // show stack trace if in development mode
+        stack: process.env.NODE_ENV === 'production' ? null : err.stack,
+    });
+};

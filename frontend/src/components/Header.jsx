fa19f6c550fee3d946273480c265a2b52124c846
simon-bertram <simonbertram@hotmail.com>
2023-10-22 10:11:44 +0100

    Add user menu dropdown when user is logged in

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,11 +1,35 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { useLogoutMutation } from '../slices/usersApiSlice';
+import { logout } from '../slices/authSlice';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Badge from 'react-bootstrap/Badge';
 import { LinkContainer } from 'react-router-bootstrap';
 import Navbar from 'react-bootstrap/Navbar';
 import { FaSignInAlt, FaSignOutAlt } from "react-icons/fa";
 import { Link } from 'react-router-dom';
+import { toast } from 'react-toastify';
 
 const Header = () => {
+  const userInfo = useSelector((state) => state.auth.userInfo);
+
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const [logoutApiCall] = useLogoutMutation();
+
+  const logoutHandler = async () => {
+    try {
+      await logoutApiCall().unwrap();
+      dispatch(logout());
+      navigate('/');
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return ( 
     <header>
       <Navbar expand="lg" className="bg-primary" data-bs-theme="dark">
@@ -13,16 +37,40 @@ const Header = () => {
           <LinkContainer to="/">
             <Navbar.Brand href="#home">MERN Auth</Navbar.Brand>
           </LinkContainer>
-          
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
-              <LinkContainer to="/login">
-                <Nav.Link><FaSignInAlt /> Login</Nav.Link>
+            <LinkContainer to="/project-code">
+                <Nav.Link>
+                  Project code
+                </Nav.Link>
               </LinkContainer>
-              <LinkContainer to="/register">
-                <Nav.Link><FaSignOutAlt /> Sign Up</Nav.Link>
-              </LinkContainer>
+
+              { userInfo ? (
+                <>
+                  <NavDropdown title={userInfo.name} id="username">
+                    <LinkContainer to="/profile">
+                      <NavDropdown.Item>
+                        Profile
+                      </NavDropdown.Item>
+                    </LinkContainer>
+                    <NavDropdown.Item onClick={logoutHandler}>
+                      Logout
+                    </NavDropdown.Item>
+                  </NavDropdown>
+                </>
+                ) 
+                : (
+                <>
+                  <LinkContainer to="/login">
+                    <Nav.Link><FaSignInAlt /> Login</Nav.Link>
+                  </LinkContainer>
+                  <LinkContainer to="/register">
+                    <Nav.Link><FaSignOutAlt /> Sign Up</Nav.Link>
+                  </LinkContainer>
+                </>
+              )}
+              
             </Nav>
           </Navbar.Collapse>
         </Container>

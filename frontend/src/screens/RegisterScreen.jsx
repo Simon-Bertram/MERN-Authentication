0636d5f33d28c4ac78b76e7ff490f0b46fd7dfc0
simon-bertram <simonbertram@hotmail.com>
2023-10-22 10:17:21 +0100

    Add useRegisterMutation functionality in the RegisterScreen

--- frontend/src/screens/RegisterScreen.jsx
+++ frontend/src/screens/RegisterScreen.jsx
@@ -1,10 +1,16 @@
 import { useState, useEffect } from 'react';
 import FormContainer from '../components/FormContainer';
 import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { useRegisterMutation } from '../slices/usersApiSlice';
+import { setCredentials } from '../slices/authSlice';
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
+import { toast } from 'react-toastify';
+import Loader from '../components/Loader';
 
 const RegisterScreen = () => {
   const [name, setName] = useState('');
@@ -12,15 +18,48 @@ const RegisterScreen = () => {
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
 
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const { userInfo } = useSelector((state) => state.auth);
+
+  const [register, { isLoading, error }] = useRegisterMutation();
+
+  useEffect(() => {
+    if (userInfo) {
+      navigate('/');
+    }
+  }, [navigate, userInfo]);
+
   const submitHandler = async (e) => { 
     e.preventDefault();
-    console.log('submit');
+    if (password !== confirmPassword) {
+      toast.error('Passwords do not match');
+      return;
+    } else {
+      try {
+        const res = await register({ name, email, password }).unwrap();
+        dispatch(setCredentials({ ...res }));
+        navigate('/');
+      } catch (error) {
+        toast.error(error?.data?.message || error.error.message);
+      }
+    }
   }
 
   return ( 
     <FormContainer>
       <h1>Register for an account</h1>
       <Form onSubmit={submitHandler}>
+        <Form.Group className='my-3' controlId='name'>
+          <Form.Label>Name</Form.Label>
+          <Form.Control
+            type='text'
+            placeholder='Enter name'
+            value={name}
+            onChange={(e) => setName(e.target.value)}>
+          </Form.Control>
+        </Form.Group>
         <Form.Group className='my-3' controlId='email'>
           <Form.Label>Email Address</Form.Label>
           <Form.Control
@@ -47,6 +86,9 @@ const RegisterScreen = () => {
             value={confirmPassword}
             onChange={(e) => setConfirmPassword(e.target.value)}>
           </Form.Control>
+
+          { isLoading && <Loader /> }
+
           <Button type='submit' variant='primary' className='my-4'>
             Register
           </Button>

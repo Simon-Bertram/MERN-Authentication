7f8f07b634ef3957109ac8cfcc191838ced3d7bb
simon-bertram <simonbertram@hotmail.com>
2023-10-23 03:59:44 +0100

    Change samesite to none

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -8,7 +8,7 @@ const generateToken = (res, userId) => {
   res.cookie('jwt', token, {
     httpOnly: true,
     secure: process.env.NODE_ENV !== 'development',
-    samesite: 'strict',
+    samesite: 'none',
     maxAge: 24 * 60 * 60 * 10000 // 10 days
   });
 };
